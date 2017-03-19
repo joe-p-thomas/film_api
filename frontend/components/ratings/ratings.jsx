6d7e4f911aff1b4c833f417ef01a8c25ec5c2c37
Joe Thomas <joe.p.thomas92@gmail.com>
2017-03-18 22:08:02 -0700

    remove star rating component, replaced with simple checkbox input

--- frontend/components/ratings/ratings.jsx
+++ frontend/components/ratings/ratings.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import StarRatingComponent from 'react-star-rating-component';
 
 class Ratings extends React.Component {
   constructor(props) {
@@ -8,36 +7,48 @@ class Ratings extends React.Component {
       score: this.props.film.userRating.score,
       film_id: this.props.film.id
     };
-
     this.handleInput = this.handleInput.bind(this);
   }
 
-  handleInput(rating) {
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.film) {
+      this.setState({score: nextProps.film.userRating.score});
+    }
+  }
+
+  handleInput(e) {
     if (this.state.score === 0) {
       this.props.submitRating({
-        score: rating,
+        score: e.target.value,
         film_id: this.props.film.id
       });
     } else {
       this.props.updateRating({
         id: this.props.film.userRating.id,
-        score: rating
+        score: e.target.value
       });
     }
-    this.setState({score: rating});
+    this.setState({score: e.target.value});
   }
 
   render() {
+    let buttons = [1,2,3,4,5];
+    buttons = buttons.map(i => (
+      <input type="checkbox"
+             name="score"
+             value={i}
+             label={i}
+             checked={this.state.score >= i}
+             key={i}
+             className='rating-button'
+             onChange={this.handleInput}/>
+    ));
     return(
       <div>
         <h3>Rate this Film:</h3>
-        <StarRatingComponent
-          name="rating"
-          starCount={5}
-          value={this.props.film.userRating.score}
-          onStarClick={this.handleInput}
-          starColor={'#e6994c'}
-          emptyStarColor={'#ccc'}/>
+          <form>
+            1 {buttons} 5
+          </form>
       </div>
     );
   }
